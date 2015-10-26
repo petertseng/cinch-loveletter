1a12b6f691535de6feab0f9c357a714144ef9583
Peter Tseng <pht24@cornell.edu>
2015-10-26 00:31:01 -0700

    rspec: rm bot nick
    The default works fine

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -29,12 +29,7 @@ RSpec.describe Cinch::Plugins::LoveLetter do
     :settings => '/dev/null',
   }}
   let(:bot) {
-    b = make_bot(described_class, opts) { |c|
-      self.loggers.first.level = :warn
-    }
-    # No, c.nick = 'testbot' doesn't work because... isupport?
-    allow(b).to receive(:nick).and_return('testbot')
-    b
+    make_bot(described_class, opts) { |c| c.loggers.first.level = :warn }
   }
   let(:plugin) { bot.plugins.first }
 
